68954ab1a33ead09b54db08c65809e68cfb81574
juankman94 <juan.carlos.menonita@gmail.com>
2018-11-17 19:51:18 -0600

    Update About page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <section id="app">
+    <nav id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/accounts">Accounts</router-link> |
       <router-link to="/expenses">Expenses</router-link>
-    </div>
+    </nav>
     <router-view/>
-  </div>
+  </section>
 </template>
 
 <style lang="scss">
@@ -15,11 +15,13 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+	width: 90%;
+	margin: 1rem auto;
 }
 #nav {
   padding: 30px;
+	text-align: center;
   a {
     font-weight: bold;
     color: #2c3e50;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<article class="about">
+		<h2>Finances Baking (subject to change)</h2>
+
+		<p>Expenses record keeper without a back-end (<i>yet</i>, more on that later)
+		&mdash; <strong>easy to use, easy to erase</strong>.</p>
+
+		<p>It's hard keeping record on what you spend money, and if you're a bit
+		paranoid with your phone and apps, it continues to be hard. We're writing
+		this simple application to keep a record and easily dispose the information
+		&mdash; <b>or not!</b> We're also writing a cloud storage for the information,
+		but that comes later.</p>
+
+		<p>The <b>first phase</b> of the project consists of a
+		<abbr title="Progressive Web Application">PWA</abbr> using
+		<a href="https://vuejs.org">Vue</a> and browsers' IndexedDB through
+		<a href="https://dexie.org">Dexie.js</a>.</p>
+
+		<p>The <b>second phase</b> will be a service to store the records safely
+		in the cloud.</p>
+
+		<p>The <b>third phase</b> will be a native android application.</p>
+	</article>
 </template>
