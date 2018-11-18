f444d0332ea771df91253c9691b6a0ab3a3b1469
juankman94 <juan.carlos.menonita@gmail.com>
2018-11-17 19:17:30 -0600

    Add route for expenses component

---
+++ src/views/Expenses.vue
@@ -0,0 +1,19 @@
+<template>
+	<section class="expenses">
+		<h2>Expenses</h2>
+		<expense-form></expense-form>
+		<expense-list></expense-list>
+	</section>
+</template>
+
+<script>
+	import ExpenseForm from '../components/ExpenseForm.vue';
+	import ExpenseList from '../components/ExpenseList.vue';
+
+	export default {
+		components: {
+			ExpenseForm,
+			ExpenseList,
+		},
+	}
+</script>
