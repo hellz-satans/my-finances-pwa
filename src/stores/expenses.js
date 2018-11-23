d815aeeab2f113b358622806c6556fd77a82e732
juankman94 <juan.carlos.menonita@gmail.com>
2018-11-23 10:18:32 -0600

    Sort expenses by date in decreasing order by default

--- src/stores/expenses.js
+++ src/stores/expenses.js
@@ -25,10 +25,10 @@ const ExpensesStore = {
       db.expenses.toArray()
         .then((arr) => {
           if (options.sort) {
-            arr.sort((a, b) => moment(a.date) - moment(b.date));
-          } else {
             // sort by date in decreasing order
             arr.sort((a, b) => moment(b.date) - moment(a.date));
+          } else {
+            arr.sort((a, b) => moment(a.date) - moment(b.date));
           }
           return arr;
         }).then((arr) => {
