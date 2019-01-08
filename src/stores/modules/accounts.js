901a888679ba9d2b66f35616d877d96516843de3
juankman94 <juan.carlos.menonita@gmail.com>
2019-01-08 17:19:28 -0600

    [indent] accounts & expenses stores

--- src/stores/modules/accounts.js
+++ src/stores/modules/accounts.js
@@ -1,106 +1,106 @@
 import db from '@/db';
 
 const AccountsStore = {
-  state: {
-    currentAccount: {
-      name: null,
-      balance: 0,
-      color: '#1fbc9c',
-    },
-    openModal: false,
-    accounts: [],
-  },
+	state: {
+		currentAccount: {
+			name: null,
+			balance: 0,
+			color: '#1fbc9c',
+		},
+		openModal: false,
+		accounts: [],
+	},
 
-  mutations: {
-    getAccounts(state) {
-      db.accounts.toArray()
-        .then(arr => state.accounts = arr)
-        .catch((err) => {
-          console.error('getAccounts:', err);
-          throw err;
-        });
-    },
-    setCurrentAccount(state, id) {
-      db.accounts.get(id)
-        .then((account) => {
-          state.currentAccount = account;
-          return id;
-        })
-        .catch((err) => {
-          console.warn('account not found:', id);
-          throw err;
-        });
-    },
-    unsetCurrentAccount(state) {
-      if (state.currentAccount.id) {
-        state.currentAccount = {};
-      }
-    },
-    updateCurrentAccountName(state, name) {
-      state.currentAccount.name = name;
-    },
-    updateCurrentAccountBalance(state, balance) {
-      state.currentAccount.balance = Number(balance);
-    },
-    updateCurrentAccountColor(state, color) {
-      state.currentAccount.color = color;
-    },
-    toggleModal(state) {
-      state.openModal = !state.openModal;
-    },
-  },
+	mutations: {
+		getAccounts(state) {
+			db.accounts.toArray()
+				.then(arr => state.accounts = arr)
+				.catch((err) => {
+					console.error('getAccounts:', err);
+					throw err;
+				});
+		},
+		setCurrentAccount(state, id) {
+			db.accounts.get(id)
+				.then((account) => {
+					state.currentAccount = account;
+					return id;
+				})
+				.catch((err) => {
+					console.warn('account not found:', id);
+					throw err;
+				});
+		},
+		unsetCurrentAccount(state) {
+			if (state.currentAccount.id) {
+				state.currentAccount = {};
+			}
+		},
+		updateCurrentAccountName(state, name) {
+			state.currentAccount.name = name;
+		},
+		updateCurrentAccountBalance(state, balance) {
+			state.currentAccount.balance = Number(balance);
+		},
+		updateCurrentAccountColor(state, color) {
+			state.currentAccount.color = color;
+		},
+		toggleModal(state) {
+			state.openModal = !state.openModal;
+		},
+	},
 
-  actions: {
-    toggleModal({ commit }, unsetAccount = false) {
-      if (unsetAccount) {
-        commit('unsetCurrentAccount');
-      }
-      commit('toggleModal');
-    },
-    createAccount({ commit }, account) {
-      return db.accounts.add(account);
-    },
-    editAccount({ commit }, id) {
-      commit('setCurrentAccount', id);
-      commit('toggleModal');
-    },
-    submitAccount({ commit, dispatch, state }, data) {
-      const actionName = (state.currentAccount.id)
-        ? 'updateAccount'
-        : 'createAccount';
+	actions: {
+		toggleModal({ commit }, unsetAccount = false) {
+			if (unsetAccount) {
+				commit('unsetCurrentAccount');
+			}
+			commit('toggleModal');
+		},
+		createAccount({ commit }, account) {
+			return db.accounts.add(account);
+		},
+		editAccount({ commit }, id) {
+			commit('setCurrentAccount', id);
+			commit('toggleModal');
+		},
+		submitAccount({ commit, dispatch, state }, data) {
+			const actionName = (state.currentAccount.id)
+				? 'updateAccount'
+				: 'createAccount';
 
-      return dispatch(actionName, data)
-        .then((id) => {
-          commit('getAccounts');
-          commit('unsetCurrentAccount');
-          commit('toggleModal');
-          return id;
-        });
-    },
-    updateAccount({ commit, state }, data) {
-      commit('toggleModal');
-      return db.accounts.update(state.currentAccount.id, data);
-    },
-    deleteAccount({ commit }, id) {
-      // on success, resolves with an undefined result
-      return db.accounts.delete(id)
-        .then((whatever) => {
-          commit('getAccounts');
-          return whatever;
-        });
-    },
-  },
+			return dispatch(actionName, data)
+				.then((id) => {
+					commit('getAccounts');
+					commit('unsetCurrentAccount');
+					commit('toggleModal');
+					return id;
+				});
+		},
+		updateAccount({ commit, state }, data) {
+			commit('toggleModal');
+			return db.accounts.update(state.currentAccount.id, data);
+		},
+		deleteAccount({ commit }, id) {
+			// on success, resolves with an undefined result
+			return db.accounts.delete(id)
+				.then((whatever) => {
+					commit('getAccounts');
+					return whatever;
+				});
+		},
+	},
 
-  getters: {
-    totalBalance: (state) => {
-      if (state.accounts.length === 0)
-        return 0;
+	getters: {
+		totalBalance: (state) => {
+			if (state.accounts.length === 0)
+				return 0;
 
-      return state.accounts
-        .map(acc => acc.balance)
-        .reduce((total, curr) => total + curr);
-    },
-  },
+			return state.accounts
+				.map(acc => acc.balance)
+				.reduce((total, curr) => total + curr);
+		},
+	},
 };
 
 export { AccountsStore };
--- src/stores/modules/expenses.js
+++ src/stores/modules/expenses.js
@@ -15,209 +15,210 @@ function newExpense () {
 }
 
 const ExpensesStore = {
-  namespaced: true,
+	namespaced: true,
 
-  state: {
-    currentExpense: {
-      description: null,
-      price: 0,
-      qty: 1,
-      date: moment().format(),
-      tags: [],
-      category: 'other',
-      subcategory: 'other_other',
-    },
-    expenses: [],
-    expensesFilters: [],
-    openModal: false,
-  },
+	state: {
+		currentExpense: {
+			description: null,
+			price: 0,
+			qty: 1,
+			date: moment().format(),
+			tags: [],
+			category: 'other',
+			subcategory: 'other_other',
+		},
+		expenses: [],
+		expensesFilters: [],
+		openModal: false,
+	},
 
-  mutations: {
-    setNewExpense(state) {
-      state.expense = newExpense;
-    },
-    getExpenses(state, options = {}) {
-      options.sort = options.sort || 1;
+	mutations: {
+		setNewExpense(state) {
+			state.expense = newExpense;
+		},
+		getExpenses(state, options = {}) {
+			options.sort = options.sort || 1;
 
-      db.expenses.toArray()
-        .then((arr) => {
-          if (options.sort) {
-            // sort by date in decreasing order
-            arr.sort((a, b) => moment(b.date) - moment(a.date));
-          } else {
-            arr.sort((a, b) => moment(a.date) - moment(b.date));
-          }
-          return arr;
-        }).then((arr) => {
-          return arr.filter((exp) => filterExpenses(exp, state.expensesFilters));
-        }).then((arr) => {
-          state.expenses = arr;
-        }).catch((err) => {
-          console.error('getExpenses:', err);
-          throw err;
-        });
-    },
-    setCurrentExpense(state, id) {
-      return db.expenses.get(id)
-        .then((expense) => {
-          expense.date = moment(expense.date).format();
-          state.currentExpense = expense;
-          return id;
-        })
-        .catch((err) => {
-          console.warn('expense not found:', id);
-          throw err;
-        });
-    },
-    unsetCurrentExpense(state) {
-      if (state.currentExpense.id) {
-        state.currentExpense = {};
-      }
-    },
-    updateCurrentExpenseDescription(state, description) {
-      state.currentExpense.description = description;
-    },
-    updateCurrentExpensePrice(state, price) {
-      state.currentExpense.price = Number(price);
-    },
-    updateCurrentExpenseQty(state, qty) {
-      state.currentExpense.qty = Number(qty);
-    },
-    updateCurrentExpenseCategory(state, category) {
-      state.currentExpense.category = category;
-    },
-    updateCurrentExpenseSubcategory(state, subcategory) {
-      state.currentExpense.subcategory = subcategory;
-    },
-    updateCurrentExpenseDate(state, date) {
-      state.currentExpense.date = date;
-    },
-    toggleModal(state) {
-      state.openModal = !state.openModal;
-    },
-  },
+			db.expenses.toArray()
+				.then((arr) => {
+					if (options.sort) {
+						// sort by date in decreasing order
+						arr.sort((a, b) => moment(b.date) - moment(a.date));
+					} else {
+						arr.sort((a, b) => moment(a.date) - moment(b.date));
+					}
+					return arr;
+				}).then((arr) => {
+					return arr.filter((exp) => filterExpenses(exp, state.expensesFilters));
+				}).then((arr) => {
+					state.expenses = arr;
+				}).catch((err) => {
+					console.error('getExpenses:', err);
+					throw err;
+				});
+		},
+		setCurrentExpense(state, id) {
+			return db.expenses.get(id)
+				.then((expense) => {
+					expense.date = moment(expense.date).format();
+					state.currentExpense = expense;
+					return id;
+				})
+				.catch((err) => {
+					console.warn('expense not found:', id);
+					throw err;
+				});
+		},
+		unsetCurrentExpense(state) {
+			if (state.currentExpense.id) {
+				state.currentExpense = {};
+			}
+		},
+		updateCurrentExpenseDescription(state, description) {
+			state.currentExpense.description = description;
+		},
+		updateCurrentExpensePrice(state, price) {
+			state.currentExpense.price = Number(price);
+		},
+		updateCurrentExpenseQty(state, qty) {
+			state.currentExpense.qty = Number(qty);
+		},
+		updateCurrentExpenseCategory(state, category) {
+			state.currentExpense.category = category;
+		},
+		updateCurrentExpenseSubcategory(state, subcategory) {
+			state.currentExpense.subcategory = subcategory;
+		},
+		updateCurrentExpenseDate(state, date) {
+			state.currentExpense.date = date;
+		},
+		toggleModal(state) {
+			state.openModal = !state.openModal;
+		},
+	},
 
-  actions: {
-    toggleModal({ commit }, unsetExpense) {
-      if(unsetExpense) {
-        commit('unsetCurrentExpense');
-      }
-      commit('toggleModal');
-    },
-    createExpense({ commit }, expense) {
-      expense.date = new Date(expense.date);
-      return db.expenses.add(expense);
-    },
-    editExpense({ commit }, id) {
-      commit('setCurrentExpense', id);
-      commit('toggleModal');
-    },
-    /**
-     * Submit current expense for storage.
-     */
-    submitExpense({ commit, dispatch, state }) {
-      const actionName = (state.currentExpense.id)
-        ? 'updateExpense'
-        : 'createExpense';
-      const data = {
-		  category: state.currentExpense.category,
-		  date: state.currentExpense.date,
-		  description: state.currentExpense.description,
-		  price: state.currentExpense.price,
-		  qty: state.currentExpense.qty,
-		  subcategory: state.currentExpense.subcategory
-	  }
+	actions: {
+		toggleModal({ commit }, unsetExpense) {
+			if(unsetExpense) {
+				commit('unsetCurrentExpense');
+			}
+			commit('toggleModal');
+		},
+		createExpense({ commit }, expense) {
+			expense.date = new Date(expense.date);
+			return db.expenses.add(expense);
+		},
+		editExpense({ commit }, id) {
+			commit('setCurrentExpense', id);
+			commit('toggleModal');
+		},
+		/**
+		 * Submit current expense for storage.
+		 */
+		submitExpense({ commit, dispatch, state }) {
+			const actionName = (state.currentExpense.id)
+				? 'updateExpense'
+				: 'createExpense';
+			const data = {
+				category: state.currentExpense.category,
+				date: state.currentExpense.date,
+				description: state.currentExpense.description,
+				price: state.currentExpense.price,
+				qty: state.currentExpense.qty,
+				subcategory: state.currentExpense.subcategory
+			}
 
-      return dispatch(actionName, data)
-        .then((id) => {
-          commit('getExpenses');
-          commit('unsetCurrentExpense');
-          commit('toggleModal');
-          commit('setNewExpense');
-          return id;
-        });
-    },
-    updateExpense({ commit, state }, data) {
-      // convert date input to Date type
-      if (data.date) {
-        data.date = new Date(data.date);
-      }
+			return dispatch(actionName, data)
+				.then((id) => {
+					commit('getExpenses');
+					commit('unsetCurrentExpense');
+					commit('toggleModal');
+					commit('setNewExpense');
+					return id;
+				});
+		},
+		updateExpense({ commit, state }, data) {
+			// convert date input to Date type
+			if (data.date) {
+				data.date = new Date(data.date);
+			}
 
-      return db.expenses.update(state.currentExpense.id, data);
-    },
-    deleteExpense({ commit }, id) {
-      // on success, resolves with an undefined result
-      return db.expenses.delete(id)
-        .then((whatever) => {
-          commit('getExpenses');
-          return whatever;
-        });
-    },
-    filterExpenses({ commit, state }, data) {
-      const filters = [];
-      if (data.startDate) {
-        filters.push({ field: 'date', op: '>=', value: new Date(data.startDate) });
-      }
+			return db.expenses.update(state.currentExpense.id, data);
+		},
+		deleteExpense({ commit }, id) {
+			// on success, resolves with an undefined result
+			return db.expenses.delete(id)
+				.then((whatever) => {
+					commit('getExpenses');
+					return whatever;
+				});
+		},
+		filterExpenses({ commit, state }, data) {
+			const filters = [];
+			if (data.startDate) {
+				filters.push({ field: 'date', op: '>=', value: new Date(data.startDate) });
+			}
 
-      if (data.endDate) {
-        filters.push({ field: 'date', op: '<=', value: new Date(data.endDate) });
-      }
+			if (data.endDate) {
+				filters.push({ field: 'date', op: '<=', value: new Date(data.endDate) });
+			}
 
-      if (data.price) {
-        filters.push({
-          field: 'price',
-          op: (data.comparator === '<=') ? '<=' : '>=',
-          value: data.price
-        });
-      }
+			if (data.price) {
+				filters.push({
+					field: 'price',
+					op: (data.comparator === '<=') ? '<=' : '>=',
+					value: data.price
+				});
+			}
 
-      state.expensesFilters = filters;
-      commit('getExpenses');
-    },
-    /**
-     * Seed random data to the DB.
-     */
-    seedData({ commit, state }, data) {
-      db.expenses
-        .toArray()
-        .then((arr) => {
-          if (arr.length > 0)
-            return undefined;
+			state.expensesFilters = filters;
+			commit('getExpenses');
+		},
 
-          const n = 15;
-          let i = 0,
-              j = 0,
-              expense = {};
-          for (i = 0; i < n; ++i) {
-            j = Math.random() * (20 - 1) + 1; // day range
-            expense.description = `Expense ${i}`;
-            expense.price = Math.floor(Math.random() * (100 - 20) + 20);
-            expense.qty = 1;
-            expense.category = 'other';
-            expense.subcategory = 'other_other';
-            if (Math.random() < 0.50) {
-              expense.date = new Date(moment().subtract(j, 'd').toISOString());
-            } else {
-              expense.date = new Date(moment().add(j, 'd').toISOString());
-            }
+		/**
+		 * Seed random data to the DB.
+		 */
+		seedData({ commit, state }, data) {
+			db.expenses
+				.toArray()
+				.then((arr) => {
+					if (arr.length > 0)
+						return undefined;
 
-            db.expenses.add(expense);
-          }
-        })
-        .then(() => commit('getExpenses'));
-    },
-  },
+					const n = 15;
+					let i = 0,
+						j = 0,
+						expense = {};
+					for (i = 0; i < n; ++i) {
+						j = Math.random() * (20 - 1) + 1; // day range
+						expense.description = `Expense ${i}`;
+						expense.price = Math.floor(Math.random() * (100 - 20) + 20);
+						expense.qty = 1;
+						expense.category = 'other';
+						expense.subcategory = 'other_other';
+						if (Math.random() < 0.50) {
+							expense.date = new Date(moment().subtract(j, 'd').toISOString());
+						} else {
+							expense.date = new Date(moment().add(j, 'd').toISOString());
+						}
 
-  getters: {
-    totalExpenses: (state) => {
-      if (state.expenses.length === 0)
-        return 0;
+						db.expenses.add(expense);
+					}
+				})
+				.then(() => commit('getExpenses'));
+		},
+	},
 
-      return state.expenses
-        .map(expense => expense.price * expense.qty)
-        .reduce((total, curr) => total + curr);
-    },
-  }
+	getters: {
+		totalExpenses: (state) => {
+			if (state.expenses.length === 0)
+				return 0;
+
+			return state.expenses
+				.map(expense => expense.price * expense.qty)
+				.reduce((total, curr) => total + curr);
+		},
+	}
 };
 
 export { ExpensesStore };
