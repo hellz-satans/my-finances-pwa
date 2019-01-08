bce93f442d2cbca82785b037b246a227bc607669
juankman94 <juan.carlos.menonita@gmail.com>
2019-01-08 17:14:18 -0600

    [indent] stores/index.js

--- src/stores/index.js
+++ src/stores/index.js
@@ -6,64 +6,60 @@ import { downloadJsonUsingBrowser } from '@/download'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: {
-    accounts: {
-      namespaced: true,
-      ... Accounts,
-    },
-    categories: {
-      namespaced: true,
-      ... Categories,
-    },
-    expenses: {
-      namespaced: true,
-      ... Expenses,
-    },
-  },
+	modules: {
+		accounts: {
+			namespaced: true,
+			... Accounts,
+		},
+		categories: {
+			namespaced: true,
+			... Categories,
+		},
+		expenses: {
+			namespaced: true,
+			... Expenses,
+		},
+	},
 
-  state: {
-    // TODO: get goal & categories from db
-    goal: 200000,
-    categories: [
-      'food', 'dinner', 'transport',
-      'date', 'friends', 'others'
-    ]
-  },
+	state: {
+		// TODO: get goal
+		goal: 200000
+	},
 
-  mutations: {
-  },
+	mutations: {
+	},
 
-  actions: {
-    /**
-     * Export data in a secure fashion.
-     *
-     * Maybe ask the user for a passphrase or something like that.
-     * First encode data and then cipher it, see
-     * https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
-     */
-    exportData({ state }) {
-      const data = {
-        expenses: state.expenses.expenses,
-        accounts: state.accounts.accounts,
-      };
+	actions: {
+		/**
+		 * Export data in a secure fashion.
+		 *
+		 * Maybe ask the user for a passphrase or something like that.
+		 * First encode data and then cipher it, see
+		 * https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
+		 */
+		exportData({ state }) {
+			const data = {
+				expenses: state.expenses.expenses,
+				accounts: state.accounts.accounts,
+			};
 
-      downloadJsonUsingBrowser('my_finances.json', data)
-    },
+			downloadJsonUsingBrowser('my_finances.json', data)
+		},
 
-    /**
-     * Import secured data.
-     *
-     * Maybe ask the user for a passphrase or something like that and
-     * decipher before inserting.
-     */
-    importData(ctx, data) {
-      console.debug('Bleep-bloop, importing data');
-    },
-  },
+		/**
+		 * Import secured data.
+		 *
+		 * Maybe ask the user for a passphrase or something like that and
+		 * decipher before inserting.
+		 */
+		importData(ctx, data) {
+			console.debug('Bleep-bloop, importing data', data);
+		},
+	},
 
-  getters: {
-    balanceSummary: (state, getters, rootGetters) => {
+	getters: {
+		balanceSummary: (state, getters, rootGetters) => {
 			return getters['accounts/totalBalance'] - getters['expenses/totalExpenses'];
-    },
-  }
+		},
+	}
 })
