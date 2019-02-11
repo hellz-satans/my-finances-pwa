9cb5e6f3692d867637a82833218458a8e3cb468c
juankman94 <juan.carlos.menonita@gmail.com>
2019-02-10 18:21:30 -0600

    [indent] using 2 spaces instead of hard-tabs

--- src/components/ExpenseForm.vue
+++ src/components/ExpenseForm.vue
@@ -1,172 +1,172 @@
 <template>
-	<article>
-		<sui-button @click.native="toggleModal(true)" v-if="!hideBtn" positive>Add expense</sui-button>
+  <article>
+    <sui-button @click.native="toggleModal(true)" v-if="!hideBtn" positive>Add expense</sui-button>
 
-		<sui-modal v-model="expenseModal" size="tiny">
-			<sui-modal-header>Add expense</sui-modal-header>
+    <sui-modal v-model="expenseModal" size="tiny">
+      <sui-modal-header>Add expense</sui-modal-header>
 
-			<sui-modal-content scrolling>
-				<sui-form name="expense" method="POST">
-					<sui-form-field>
-						<label for="price">Price</label>
-						<sui-input
-							type="number"
-							name="price"
-							min="0.1"
-							step="any"
-							required
-							v-model="price"
-						/>
+      <sui-modal-content scrolling>
+        <sui-form name="expense" method="POST">
+          <sui-form-field>
+            <label for="price">Price</label>
+            <sui-input
+              type="number"
+              name="price"
+              min="0.1"
+              step="any"
+              required
+              v-model="price"
+            />
             <p v-for="(err, i) in expenseErrors.price" :key="i" class="red text">
               {{ err }}
             </p>
-					</sui-form-field>
+          </sui-form-field>
 
-					<sui-form-field>
-						<label for="date">Date</label>
-						<datetime v-model="date" type="datetime" />
+          <sui-form-field>
+            <label for="date">Date</label>
+            <datetime v-model="date" type="datetime" />
             <p v-for="(err, i) in expenseErrors.date" :key="i" class="red text">
               {{ err }}
             </p>
-					</sui-form-field>
+          </sui-form-field>
 
-					<sui-form-field>
-						<label for="qty">Quantity</label>
-						<sui-input
-							type="number"
-							name="qty"
-							min="1"
-							step="1"
-							required
-							v-model="qty"
-						/>
+          <sui-form-field>
+            <label for="qty">Quantity</label>
+            <sui-input
+              type="number"
+              name="qty"
+              min="1"
+              step="1"
+              required
+              v-model="qty"
+            />
             <p v-for="(err, i) in expenseErrors.qty" :key="i" class="red text">
               {{ err }}
             </p>
-					</sui-form-field>
+          </sui-form-field>
 
-					<sui-form-field>
-						<label for="category">Category</label>
-						<sui-dropdown
-							placeholder="Category"
-							fluid
-							selection
-							:options="expenseCategories"
-							v-model="category"
-						/>
+          <sui-form-field>
+            <label for="category">Category</label>
+            <sui-dropdown
+              placeholder="Category"
+              fluid
+              selection
+              :options="expenseCategories"
+              v-model="category"
+            />
             <p v-for="(err, i) in expenseErrors.category" :key="i" class="red text">
               {{ err }}
             </p>
-					</sui-form-field>
+          </sui-form-field>
 
-					<sui-form-field>
-						<label for="subcategory">Subcategory</label>
-						<sui-dropdown
-							placeholder="Subcategory"
-							fluid
-							selection
-							:options="expenseSubcategories"
-							v-model="subcategory"
-						/>
+          <sui-form-field>
+            <label for="subcategory">Subcategory</label>
+            <sui-dropdown
+              placeholder="Subcategory"
+              fluid
+              selection
+              :options="expenseSubcategories"
+              v-model="subcategory"
+            />
             <p v-for="(err, i) in expenseErrors.subcategory" :key="i" class="red text">
               {{ err }}
             </p>
-					</sui-form-field>
+          </sui-form-field>
 
-					<sui-form-field>
-						<label for="description">Description</label>
-						<sui-input
-							name="description"
-							minlength="1"
-							pattern=".+"
-							required
-							v-model="description"
-						/>
+          <sui-form-field>
+            <label for="description">Description</label>
+            <sui-input
+              name="description"
+              minlength="1"
+              pattern=".+"
+              required
+              v-model="description"
+            />
             <p v-for="(err, i) in expenseErrors.description" :key="i" class="red text">
               {{ err }}
             </p>
-					</sui-form-field>
-				</sui-form>
-			</sui-modal-content>
+          </sui-form-field>
+        </sui-form>
+      </sui-modal-content>
 
-			<sui-modal-actions>
-				<sui-button @click="toggleModal">Close</sui-button>
-				<sui-button positive @click="submitExpense">{{ action }}</sui-button>
-			</sui-modal-actions>
-		</sui-modal>
-	</article>
+      <sui-modal-actions>
+        <sui-button @click="toggleModal">Close</sui-button>
+        <sui-button positive @click="submitExpense">{{ action }}</sui-button>
+      </sui-modal-actions>
+    </sui-modal>
+  </article>
 </template>
 
 <script>
-	import { mapMutations, mapActions, mapGetters, mapState } from 'vuex'
-	import { Datetime } from 'vue-datetime'
+import { mapMutations, mapActions, mapGetters, mapState } from 'vuex'
+import { Datetime } from 'vue-datetime'
 
-	export default {
-		name: 'ExpenseForm',
-		props: ['hideBtn'],
-		components: {
-			datetime: Datetime,
-		},
-		methods: {
-			... mapMutations('expenses', [ 'unsetCurrentExpense' ]),
-			... mapActions('expenses', [ 'toggleModal' ]),
-			submitExpense() {
-        this.$store.dispatch('expenses/submitExpense')
-          .catch((err) => {
-            console.error('expenseForm:', err, err.stack)
-          })
-			}
-		},
-		computed: {
-			... mapGetters('categories', [ 'expenseCategories' ]),
-			... mapState('categories', [ 'categories', 'subcategories' ]),
-			... mapState('expenses', [ 'currentExpense', 'expenseErrors', 'openModal' ]),
-			expenseSubcategories() {
-				const list = []
-				let c = null
+export default {
+  name: 'ExpenseForm',
+  props: ['hideBtn'],
+  components: {
+    datetime: Datetime,
+  },
+  methods: {
+    ... mapMutations('expenses', [ 'unsetCurrentExpense' ]),
+    ... mapActions('expenses', [ 'toggleModal' ]),
+    submitExpense() {
+      this.$store.dispatch('expenses/submitExpense')
+        .catch((err) => {
+          console.error('expenseForm:', err, err.stack)
+        })
+    }
+  },
+  computed: {
+    ... mapGetters('categories', [ 'expenseCategories' ]),
+    ... mapState('categories', [ 'categories', 'subcategories' ]),
+    ... mapState('expenses', [ 'currentExpense', 'expenseErrors', 'openModal' ]),
+    expenseSubcategories() {
+      const list = []
+      let c = null
 
-				for (const i in this.subcategories) {
-					c = this.subcategories[i]
-					if (c.key.startsWith(this.category)) {
-						list.push({ key: c.key, text: c.name, value: c.key, icon: c.icon })
-					}
-				}
+      for (const i in this.subcategories) {
+        c = this.subcategories[i]
+        if (c.key.startsWith(this.category)) {
+          list.push({ key: c.key, text: c.name, value: c.key, icon: c.icon })
+        }
+      }
 
-				return list
-			},
-			action() {
-				return this.currentExpense.id ? 'Update' : 'Save'
-			},
-			description: {
-				get() { return this.$store.state.expenses.currentExpense.description },
-				set(v) { this.$store.commit('expenses/updateCurrentExpenseDescription', v) },
-			},
-			price: {
-				get() { return this.$store.state.expenses.currentExpense.price },
-				set(v) { this.$store.commit('expenses/updateCurrentExpensePrice', v) },
-			},
-			qty: {
-				get() { return this.$store.state.expenses.currentExpense.qty },
-				set(v) { this.$store.commit('expenses/updateCurrentExpenseQty', v) },
-			},
-			category: {
-				get() { return this.$store.state.expenses.currentExpense.category },
-				set(v) { this.$store.commit('expenses/updateCurrentExpenseCategory', v) },
-			},
-			subcategory: {
-				get() { return this.$store.state.expenses.currentExpense.subcategory },
-				set(v) { this.$store.commit('expenses/updateCurrentExpenseSubcategory', v) },
-			},
-			date: {
-				// TODO: fix warning when setting the input[type=date] with this
-				get() { return this.$store.state.expenses.currentExpense.date },
-				set(v) { this.$store.commit('expenses/updateCurrentExpenseDate', v) },
-			},
+      return list
+    },
+    action() {
+      return this.currentExpense.id ? 'Update' : 'Save'
+    },
+    description: {
+      get() { return this.$store.state.expenses.currentExpense.description },
+      set(v) { this.$store.commit('expenses/updateCurrentExpenseDescription', v) },
+    },
+    price: {
+      get() { return this.$store.state.expenses.currentExpense.price },
+      set(v) { this.$store.commit('expenses/updateCurrentExpensePrice', v) },
+    },
+    qty: {
+      get() { return this.$store.state.expenses.currentExpense.qty },
+      set(v) { this.$store.commit('expenses/updateCurrentExpenseQty', v) },
+    },
+    category: {
+      get() { return this.$store.state.expenses.currentExpense.category },
+      set(v) { this.$store.commit('expenses/updateCurrentExpenseCategory', v) },
+    },
+    subcategory: {
+      get() { return this.$store.state.expenses.currentExpense.subcategory },
+      set(v) { this.$store.commit('expenses/updateCurrentExpenseSubcategory', v) },
+    },
+    date: {
+      // TODO: fix warning when setting the input[type=date] with this
+      get() { return this.$store.state.expenses.currentExpense.date },
+      set(v) { this.$store.commit('expenses/updateCurrentExpenseDate', v) },
+    },
 
-			expenseModal: {
-				get() { return this.$store.state.expenses.openModal },
-				set(v) { return this.$store.commit('expenses/toggleModal') },
-			},
-		}
-	}
+    expenseModal: {
+      get() { return this.$store.state.expenses.openModal },
+      set(v) { return this.$store.commit('expenses/toggleModal') },
+    },
+  }
+}
 </script>
--- src/stores/modules/expenses.js
+++ src/stores/modules/expenses.js
@@ -5,48 +5,48 @@ import moment from 'moment'
 import validate from 'validate.js'
 
 function newExpense() {
-	return {
-		description: null,
-		price: 0,
-		qty: 1,
-		date: moment().format(),
-		tags: [],
-		category: 'other',
-		subcategory: 'other_other',
-	}
+  return {
+    description: null,
+    price: 0,
+    qty: 1,
+    date: moment().format(),
+    tags: [],
+    category: 'other',
+    subcategory: 'other_other',
+  }
 }
 
 const ExpensesStore = {
-	namespaced: true,
+  namespaced: true,
 
-	state: {
-		currentExpense: {
-			description: null,
-			price: 0,
-			qty: 1,
-			date: moment().format(),
-			tags: [],
-			category: 'other',
-			subcategory: 'other_other',
-		},
+  state: {
+    currentExpense: {
+      description: null,
+      price: 0,
+      qty: 1,
+      date: moment().format(),
+      tags: [],
+      category: 'other',
+      subcategory: 'other_other',
+    },
     expenseErrors: {},
-		expenses: [],
-		expensesFilters: [],
-		openModal: false,
-	},
+    expenses: [],
+    expensesFilters: [],
+    openModal: false,
+  },
 
-	mutations: {
-		createExpense(state, expense) {
-			expense.date = new Date(expense.date);
-			db.expenses.add(expense);
-		},
-		setNewExpense(state) {
-			state.expense = newExpense();
-		},
-		getExpenses(state, options = {}) {
-			options.sort = options.sort || 1;
+  mutations: {
+    createExpense(state, expense) {
+      expense.date = new Date(expense.date);
+      db.expenses.add(expense);
+    },
+    setNewExpense(state) {
+      state.expense = newExpense();
+    },
+    getExpenses(state, options = {}) {
+      options.sort = options.sort || 1;
 
-			db.expenses.toArray()
+      db.expenses.toArray()
         .then((arr) => {
           // convert dates to moment instances
           for (const exp of arr) {
@@ -54,100 +54,100 @@ const ExpensesStore = {
           }
           return arr
         })
-				.then((arr) => {
-					if (options.sort) {
-						// sort by date in decreasing order
-						arr.sort((a, b) => b.date - a.date);
-					} else {
-						arr.sort((a, b) => a.date - b.date);
-					}
-					return arr;
-				}).then((arr) => {
-					state.expenses = arr;
-				}).catch((err) => {
-					console.error('getExpenses:', err);
-					throw err;
-				});
-		},
-		setCurrentExpense(state, id) {
-			return db.expenses.get(id)
-				.then((expense) => {
-					expense.date = moment(expense.date).format();
-					state.currentExpense = expense;
-					return id;
-				})
-				.catch((err) => {
-					console.warn('expense not found:', id);
-					throw err;
-				});
-		},
-		updateCurrentExpenseDescription(state, description) {
-			if (description) {
-				description = description.trim()
-			}
+        .then((arr) => {
+          if (options.sort) {
+            // sort by date in decreasing order
+            arr.sort((a, b) => b.date - a.date);
+          } else {
+            arr.sort((a, b) => a.date - b.date);
+          }
+          return arr;
+        }).then((arr) => {
+          state.expenses = arr;
+        }).catch((err) => {
+          console.error('getExpenses:', err);
+          throw err;
+        });
+    },
+    setCurrentExpense(state, id) {
+      return db.expenses.get(id)
+        .then((expense) => {
+          expense.date = moment(expense.date).format();
+          state.currentExpense = expense;
+          return id;
+        })
+        .catch((err) => {
+          console.warn('expense not found:', id);
+          throw err;
+        });
+    },
+    updateCurrentExpenseDescription(state, description) {
+      if (description) {
+        description = description.trim()
+      }
 
-			state.currentExpense.description = description
-		},
-		updateCurrentExpensePrice(state, price) {
-			state.currentExpense.price = Number(price);
-		},
-		updateCurrentExpenseQty(state, qty) {
-			state.currentExpense.qty = Number(qty);
-		},
-		updateCurrentExpenseCategory(state, category) {
-			state.currentExpense.category = category;
-		},
-		updateCurrentExpenseSubcategory(state, subcategory) {
-			state.currentExpense.subcategory = subcategory;
-		},
-		updateCurrentExpenseDate(state, date) {
-			state.currentExpense.date = date;
-		},
-		toggleModal(state) {
-			state.openModal = !state.openModal;
-		},
-		updatePastExpenses(state) {
-			expensesInRange(1, 'week')
-				.then((total) => {
-					state.expensesPastWeek = total
-				})
-			expensesInRange(1, 'month')
-				.then((total) => {
-					state.expensesPastMonth = total
-				})
-		},
-	},
+      state.currentExpense.description = description
+    },
+    updateCurrentExpensePrice(state, price) {
+      state.currentExpense.price = Number(price);
+    },
+    updateCurrentExpenseQty(state, qty) {
+      state.currentExpense.qty = Number(qty);
+    },
+    updateCurrentExpenseCategory(state, category) {
+      state.currentExpense.category = category;
+    },
+    updateCurrentExpenseSubcategory(state, subcategory) {
+      state.currentExpense.subcategory = subcategory;
+    },
+    updateCurrentExpenseDate(state, date) {
+      state.currentExpense.date = date;
+    },
+    toggleModal(state) {
+      state.openModal = !state.openModal;
+    },
+    updatePastExpenses(state) {
+      expensesInRange(1, 'week')
+        .then((total) => {
+          state.expensesPastWeek = total
+        })
+      expensesInRange(1, 'month')
+        .then((total) => {
+          state.expensesPastMonth = total
+        })
+    },
+  },
 
-	actions: {
-		toggleModal({ commit }, unsetExpense) {
-			if (unsetExpense) {
-				commit('setNewExpense');
-			}
-			commit('toggleModal');
-		},
-		createExpense({ commit }, expense) {
-			commit('createExpense', expense)
-			window.setTimeout(ev => commit('getExpenses'), 10)
-		},
-		editExpense({ commit }, id) {
-			commit('setCurrentExpense', id);
-			commit('toggleModal');
-		},
-		/**
-		 * Submit current expense for storage.
-		 */
-		submitExpense({ commit, dispatch, state }) {
-			const actionName = (state.currentExpense.id)
-				? 'updateExpense'
-				: 'createExpense';
-			const data = {
-				category: state.currentExpense.category,
-				date: state.currentExpense.date,
-				description: state.currentExpense.description,
-				price: state.currentExpense.price,
-				qty: state.currentExpense.qty,
-				subcategory: state.currentExpense.subcategory
-			}
+  actions: {
+    toggleModal({ commit }, unsetExpense) {
+      if (unsetExpense) {
+        commit('setNewExpense');
+      }
+      commit('toggleModal');
+    },
+    createExpense({ commit }, expense) {
+      commit('createExpense', expense)
+      window.setTimeout(ev => commit('getExpenses'), 10)
+    },
+    editExpense({ commit }, id) {
+      commit('setCurrentExpense', id);
+      commit('toggleModal');
+    },
+    /**
+     * Submit current expense for storage.
+     */
+    submitExpense({ commit, dispatch, state }) {
+      const actionName = (state.currentExpense.id)
+        ? 'updateExpense'
+        : 'createExpense';
+      const data = {
+        category: state.currentExpense.category,
+        date: state.currentExpense.date,
+        description: state.currentExpense.description,
+        price: state.currentExpense.price,
+        qty: state.currentExpense.qty,
+        subcategory: state.currentExpense.subcategory
+      }
       const errors = validate(data, expenseConstraints)
 
       if (!errors) {
@@ -163,110 +163,110 @@ const ExpensesStore = {
         console.error('submitExpense:', errors)
         state.expenseErrors = errors
       }
-		},
-		updateExpense({ commit, state }, data) {
-			// convert date input to Date type
-			if (data.date) {
-				data.date = new Date(data.date);
-			}
+    },
+    updateExpense({ commit, state }, data) {
+      // convert date input to Date type
+      if (data.date) {
+        data.date = new Date(data.date);
+      }
 
-			return db.expenses.update(state.currentExpense.id, data);
-		},
-		deleteExpense({ commit }, id) {
-			// on success, resolves with an undefined result
-			return db.expenses.delete(id)
-				.then((whatever) => {
-					commit('getExpenses');
-					return whatever;
-				});
-		},
-		deleteAll({ dispatch }) {
-			db.expenses
-				.toArray()
-				.then((arr) => {
-					for (let i in arr) {
-						dispatch('deleteExpense', arr[i].id)
-					}
-				})
-				.catch((err) => {
-					console.error('expenses/deleteAll:', err)
-					throw err
-				})
-		},
+      return db.expenses.update(state.currentExpense.id, data);
+    },
+    deleteExpense({ commit }, id) {
+      // on success, resolves with an undefined result
+      return db.expenses.delete(id)
+        .then((whatever) => {
+          commit('getExpenses');
+          return whatever;
+        });
+    },
+    deleteAll({ dispatch }) {
+      db.expenses
+        .toArray()
+        .then((arr) => {
+          for (let i in arr) {
+            dispatch('deleteExpense', arr[i].id)
+          }
+        })
+        .catch((err) => {
+          console.error('expenses/deleteAll:', err)
+          throw err
+        })
+    },
 
-		updatePastExpenses({ commit }) {
-			commit('updatePastExpenses')
-		},
+    updatePastExpenses({ commit }) {
+      commit('updatePastExpenses')
+    },
 
-		/**
-		 * Seed random data to the DB.
-		 */
-		seedData({ commit, state }, data) {
-			db.expenses
-				.toArray()
-				.then((arr) => {
-					if (arr.length > 0)
-						return undefined;
+    /**
+     * Seed random data to the DB.
+     */
+    seedData({ commit, state }, data) {
+      db.expenses
+        .toArray()
+        .then((arr) => {
+          if (arr.length > 0)
+            return undefined;
 
-					const n = 15;
-					let i = 0,
-						j = 0,
-						expense = {};
-					for (i = 0; i < n; ++i) {
-						j = Math.random() * (20 - 1) + 1; // day range
-						expense.description = `Expense ${i}`;
-						expense.price = Math.floor(Math.random() * (100 - 20) + 20);
-						expense.qty = 1;
-						expense.category = 'other';
-						expense.subcategory = 'other_other';
-						if (Math.random() < 0.50) {
-							expense.date = new Date(moment().subtract(j, 'd').toISOString());
-						} else {
-							expense.date = new Date(moment().add(j, 'd').toISOString());
-						}
+          const n = 15;
+          let i = 0,
+            j = 0,
+            expense = {};
+          for (i = 0; i < n; ++i) {
+            j = Math.random() * (20 - 1) + 1; // day range
+            expense.description = `Expense ${i}`;
+            expense.price = Math.floor(Math.random() * (100 - 20) + 20);
+            expense.qty = 1;
+            expense.category = 'other';
+            expense.subcategory = 'other_other';
+            if (Math.random() < 0.50) {
+              expense.date = new Date(moment().subtract(j, 'd').toISOString());
+            } else {
+              expense.date = new Date(moment().add(j, 'd').toISOString());
+            }
 
-						db.expenses.add(expense);
-					}
-				})
-				.then(() => commit('getExpenses'));
-		},
-		importExpense({ commit, dispatch }, exp) {
-			db.expenses
-				.toArray()
-				.then((arr) => {
-					let exists = false
+            db.expenses.add(expense);
+          }
+        })
+        .then(() => commit('getExpenses'));
+    },
+    importExpense({ commit, dispatch }, exp) {
+      db.expenses
+        .toArray()
+        .then((arr) => {
+          let exists = false
 
-					exists = arr.some((el) => {
-						return el.id === exp.id && el.date === exp.date
-					})
+          exists = arr.some((el) => {
+            return el.id === exp.id && el.date === exp.date
+          })
 
-					if (!exists) {
-						dispatch('createExpense', exp)
-					}
-				})
-				.catch((err) => {
-					console.error('expenses/importExpense:', err)
-					throw err
-				})
-		},
-	},
+          if (!exists) {
+            dispatch('createExpense', exp)
+          }
+        })
+        .catch((err) => {
+          console.error('expenses/importExpense:', err)
+          throw err
+        })
+    },
+  },
 
-	getters: {
-		totalExpenses: (state) => {
-			if (state.expenses.length === 0)
-				return 0;
+  getters: {
+    totalExpenses: (state) => {
+      if (state.expenses.length === 0)
+        return 0;
 
-			return state.expenses
-				.map(expense => expense.price * expense.qty)
-				.reduce((total, curr) => total + curr);
-		},
-		expensesPastWeek: (state) => {
-			return expensesInRange(state.expenses, 1, 'week')
-		},
-		expensesPastMonth: (state) => {
-			return expensesInRange(state.expenses, 1, 'month')
-		},
-	}
+      return state.expenses
+        .map(expense => expense.price * expense.qty)
+        .reduce((total, curr) => total + curr);
+    },
+    expensesPastWeek: (state) => {
+      return expensesInRange(state.expenses, 1, 'week')
+    },
+    expensesPastMonth: (state) => {
+      return expensesInRange(state.expenses, 1, 'month')
+    },
+  }
 };
 
 export { ExpensesStore };
