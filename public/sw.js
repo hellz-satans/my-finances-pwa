d483e9d33ccea7c8da0e4a6f7aa0a7e0ee7279f8
juankman94 <juan.carlos.menonita@gmail.com>
2019-04-01 00:49:59 -0600

    Add custom service worker

---
+++ public/sw.js
@@ -0,0 +1,67 @@
+var VERSION = '0.0.1';
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(preLoad());
+});
+
+var preLoad = function() {
+  console.log("Installing web app");
+  return caches.open(VERSION).then(function(cache) {
+    return cache.addAll([ "/", "/my-finances-pwa", "/my-finances-pwa/", "offline.html" ]);
+  });
+};
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    checkResponse(event.request)
+      .catch(function() {
+        return returnFromCache(event.request);
+      })
+  );
+  event.waitUntil(addToCache(event.request));
+});
+
+var checkResponse = function(request) {
+  return new Promise(function(fulfill, reject) {
+    fetch(request).then(function(response) {
+      if (response.status !== 404) {
+        fulfill(response);
+      } else {
+        reject();
+      }
+    }, reject);
+  });
+};
+
+var addToCache = function(request) {
+  return caches.open(VERSION).then(function (cache) {
+    return fetch(request).then(function (response) {
+      console.log(response.url + " was cached");
+      return cache.put(request, response);
+    });
+  });
+};
+
+var returnFromCache = function(request) {
+  return caches.open(VERSION).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status == 404) {
+        return cache.match("offline.html");
+      } else {
+        return matching;
+      }
+    });
+  });
+};
+
+// delete old caches
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== VERSION) {
+          return caches.delete(key);
+        }
+      }));
+    });
+});
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -4,19 +4,19 @@ import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
+    ready(registration) {
       console.log(
         'App is being served from cache by a service worker.\n' +
         'For more details, visit https://goo.gl/AFskqB'
       )
     },
-    cached () {
+    cached(registration) {
       console.log('Content has been cached for offline use.')
     },
-    updated () {
+    updated(registration) {
       console.log('New content is available; please refresh.')
     },
-    offline () {
+    offline(registration) {
       console.log('No internet connection found. App is running in offline mode.')
     },
     error (error) {
