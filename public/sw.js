b170a4b0b8adf051a5170db7ec4744ad6f84b10b
juankman94 <juan.carlos.menonita@gmail.com>
2020-01-27 19:57:59 -0600

    [sw] Refacto cache handling

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,10 @@
-var VERSION = '0.0.5';
+var VERSION = '0.1.0';
 
 self.addEventListener("install", function(ev) {
-  console.log("install:", ev);
   ev.waitUntil(preLoad());
 });
 
 var preLoad = function() {
-  console.log("[preload] Installing web app");
   return caches.open(VERSION).then(function(cache) {
     return cache.addAll([ "/my-finances-pwa/", "/my-finances-pwa/offline.html" ]);
   });
@@ -14,60 +12,21 @@ var preLoad = function() {
 
 self.addEventListener("fetch", function(ev) {
   console.log("fetch:", JSON.stringify(ev));
-  ev.respondWith(
-    checkResponse(ev.request)
-      .catch(function() {
-        return returnFromCache(ev.request);
-      })
-  );
-  ev.waitUntil(addToCache(ev.request))
-    .catch(function(err) {
-      console.error("Could not add to cache");
-    });
+
+  caches.match(event.request)
+    .then((response) => {
+      if (response)
+        return response
+
+      return fetch(event.request)
+        .then((resp) => {
+          return caches.open(VERSION).then((cache) => {
+            cache.put(ev.request, resp.clone())
+            return resp
+          })
+        })
+    })
 });
-
-var checkResponse = function(request) {
-  console.log("checkResponse:", JSON.stringify(request));
-  return new Promise(function(fulfill, reject) {
-    fetch(request).then(function(response) {
-      if (response.status !== 404) {
-        fulfill(response);
-      } else {
-        reject();
-      }
-    }, reject);
-  });
-};
-
-var addToCache = function(request) {
-  console.log("addToCache:", JSON.stringify(request));
-  return caches.open(VERSION).then(function (cache) {
-    return fetch(request)
-      .then(function (response) {
-        console.log(response.url + " was cached");
-        return cache.put(request, response);
-      })
-      .catch(function(err) {
-        console.error("Could not cache " + request.url, err);
-      });
-  })
-  .catch(function(err) {
-    console.error("Could not open cache.", err);
-  });
-};
-
-var returnFromCache = function(request) {
-  console.log("returnFromCache:", request);
-  return caches.open(VERSION).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      if (!matching || matching.status == 404) {
-        return cache.match("offline.html");
-      } else {
-        return matching;
-      }
-    });
-  });
-};
 
 // delete old caches
 self.addEventListener('activate', function(ev) {
