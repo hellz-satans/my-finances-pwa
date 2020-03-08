1fceb8d1ee9fd0e437d1c953f8fadda17a9e1b89
juankman94 <juan.carlos.menonita@gmail.com>
2020-03-08 09:33:37 -0600

    [route] eager load About component/route

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import About from '@/views/About'
 import Home from './views/Home.vue'
 import ImportExport from '@/components/ImportExport.vue'
 import Expenses from './views/Expenses.vue'
@@ -26,7 +27,8 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      //component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About,
     },
   ]
 })
