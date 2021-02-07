bff3623966b0ec85656f4798862a003e6568b653
juankman94 <jcmty@outlook.com>
2021-02-07 02:11:02 -0600

    Build master

---
+++ precache-manifest.a4029c4ccb8c6c0e88ff85e3ffbdf86f.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62e2f67785db7fabbef",
+    "url": "css/app.c4376b7b.css"
+  },
+  {
+    "revision": "0e45bb028419f6dfb9db",
+    "url": "css/chunk-vendors.dd70782a.css"
+  },
+  {
+    "revision": "abbe89bd522af0765f67d19bbef382bd",
+    "url": "fonts/FiraMono-Bold.abbe89bd.ttf"
+  },
+  {
+    "revision": "ca242c592b34dacb2a8951cda703e48c",
+    "url": "fonts/FiraMono-Bold.ca242c59.eot"
+  },
+  {
+    "revision": "ea0cfb9b88c1398f840eae350e12d924",
+    "url": "fonts/FiraMono-Bold.ea0cfb9b.woff"
+  },
+  {
+    "revision": "8d876c180eb01a79be6d1d8bc121919f",
+    "url": "fonts/FiraMono-Regular.8d876c18.eot"
+  },
+  {
+    "revision": "d616f619c088ed9428d76343a2c6b2ed",
+    "url": "fonts/FiraMono-Regular.d616f619.ttf"
+  },
+  {
+    "revision": "f25e0dfc5b508f34f63724d7ff607384",
+    "url": "fonts/FiraMono-Regular.f25e0dfc.woff"
+  },
+  {
+    "revision": "5b6db7a387815eea8bec2d491683a707",
+    "url": "fonts/FiraSans-Bold.5b6db7a3.eot"
+  },
+  {
+    "revision": "6028e19089deb3cf7a60d0d00e4638ba",
+    "url": "fonts/FiraSans-Bold.6028e190.ttf"
+  },
+  {
+    "revision": "bf0aaa9c4657f053f06bef1e50208e9f",
+    "url": "fonts/FiraSans-Bold.bf0aaa9c.woff"
+  },
+  {
+    "revision": "215cae1e77b2c4818f7e058850d3265b",
+    "url": "fonts/FiraSans-BoldItalic.215cae1e.woff"
+  },
+  {
+    "revision": "e52a31177b297691a585c49bd4731452",
+    "url": "fonts/FiraSans-BoldItalic.e52a3117.ttf"
+  },
+  {
+    "revision": "fb8851fe90bb7dcd2a6b468db4a2f2f6",
+    "url": "fonts/FiraSans-BoldItalic.fb8851fe.eot"
+  },
+  {
+    "revision": "271c6d46b715703ad05e20e270a5f7e8",
+    "url": "fonts/FiraSans-Light.271c6d46.ttf"
+  },
+  {
+    "revision": "29430787e85c5dc0a9e8a164fab4a5bf",
+    "url": "fonts/FiraSans-Light.29430787.woff"
+  },
+  {
+    "revision": "d20581f8149298d49c776e9a77147860",
+    "url": "fonts/FiraSans-Light.d20581f8.eot"
+  },
+  {
+    "revision": "860c08c400da08cf7a44142c0866aafe",
+    "url": "fonts/FiraSans-LightItalic.860c08c4.woff"
+  },
+  {
+    "revision": "917a72febcbc4a5cd80455045673935f",
+    "url": "fonts/FiraSans-LightItalic.917a72fe.eot"
+  },
+  {
+    "revision": "cbbb2042d64920e9d2ae8fec84c904a8",
+    "url": "fonts/FiraSans-LightItalic.cbbb2042.ttf"
+  },
+  {
+    "revision": "181fa5a2e6e9b5730eb6fe46c30b5228",
+    "url": "fonts/FiraSans-Medium.181fa5a2.woff"
+  },
+  {
+    "revision": "54b83b537f536ffab4528d8a635f865d",
+    "url": "fonts/FiraSans-Medium.54b83b53.ttf"
+  },
+  {
+    "revision": "cdb3381ca6dbd78c96cd40196da44a9e",
+    "url": "fonts/FiraSans-Medium.cdb3381c.eot"
+  },
+  {
+    "revision": "2b70d3d73007e2afa7325cc211ca7efc",
+    "url": "fonts/FiraSans-MediumItalic.2b70d3d7.ttf"
+  },
+  {
+    "revision": "965651ed4d6b21593be02b7b27c41a4a",
+    "url": "fonts/FiraSans-MediumItalic.965651ed.woff"
+  },
+  {
+    "revision": "eacc54cb353e7dfcd05067baf7efbca6",
+    "url": "fonts/FiraSans-MediumItalic.eacc54cb.eot"
+  },
+  {
+    "revision": "12801b91616871ee8783342abc4cd110",
+    "url": "fonts/FiraSans-Regular.12801b91.eot"
+  },
+  {
+    "revision": "200d5e7cc951bbffda6945f883e3123e",
+    "url": "fonts/FiraSans-Regular.200d5e7c.woff"
+  },
+  {
+    "revision": "b0aa1958e34c16cede8af5643a9c285c",
+    "url": "fonts/FiraSans-Regular.b0aa1958.ttf"
+  },
+  {
+    "revision": "766aa84c0e9df80ecc8c62609ba35b3c",
+    "url": "index.html"
+  },
+  {
+    "revision": "e62e2f67785db7fabbef",
+    "url": "js/app.1bfab4b3.js"
+  },
+  {
+    "revision": "0e45bb028419f6dfb9db",
+    "url": "js/chunk-vendors.ac6ddd92.js"
+  },
+  {
+    "revision": "65200ea942886dd5e32922403fd5af4b",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "aa6e953ca457c416d1ee3bda769685cb",
+    "url": "offline.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
